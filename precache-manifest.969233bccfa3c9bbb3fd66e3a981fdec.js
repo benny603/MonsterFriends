f837e16163f9e81469e5a79142f798e16e658c36
iambennywu <bennywu603@gmail.com>
2020-01-16 16:26:54 +0800

    Updates

---
+++ precache-manifest.969233bccfa3c9bbb3fd66e3a981fdec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abcf3aa080e41e953cd77e655a3010e",
+    "url": "/MonsterFriends/index.html"
+  },
+  {
+    "revision": "155021e8912b2a1b38e3",
+    "url": "/MonsterFriends/static/css/main.9ce5eb7d.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/MonsterFriends/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonsterFriends/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "155021e8912b2a1b38e3",
+    "url": "/MonsterFriends/static/js/main.52a37d72.chunk.js"
+  },
+  {
+    "revision": "21da055aa35cfc238243",
+    "url": "/MonsterFriends/static/js/runtime-main.a322d37f.js"
+  }
+]);
